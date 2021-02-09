488d6e43df8509c878180fc6ead5387dc2beeb03
soobinkiki <j.sb.parkk@gmail.com>
2021-02-08 16:51:53 -0800

    building in-game page 2-8-21

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ let gameTitleContainer = document.querySelector("#gameTitleContainer")
 let mainTitle = document.querySelector("#mainTitle")
 let answerBox = document.querySelector("#answer-Box")
 let meter = document.querySelector("#meter")
+let buttonContainer = document.querySelector("#buttons")
+
 let typeArea = document.createElement('input')
 
 
@@ -20,39 +22,57 @@ const styleChange = () => {
     mainTitle.style.marginRight = "20px"
     gameTitleContainer.style.display = "inline"
     gameTitleContainer.style.textAlign = "end"
-    
-    myName.style.marginTop = "185px"
 
-    // let answerContainer = document.createElement('div')
-    // answerBox.append(answerContainer)
-    // answerContainer.style.width = '1200px'
-    // answerContainer.style.height = '170px'
-    // answerContainer.style.background = 'black'
-    // answerContainer.style.marginTop = '180px'
+    var tryAginBtn = document.createElement('button')
+    buttonContainer.append(tryAginBtn)
+    tryAginBtn.innerText = "TRY AGAIN"
+    tryAginBtn.setAttribute("id", "tryAgainBtn")
+    var goBackBtn = document.createElement('button')
+    buttonContainer.append(goBackBtn)
+    goBackBtn.innerText = "MAIN"
+    goBackBtn.setAttribute("id", "goBackBtn")
 
-    typeArea.setAttribute("type", "text", "placeholder")
-    typeArea.placeholder = "NUMBERS HERE..."
+    myName.style.marginTop = "191px"
+
     answerBox.append(typeArea)
-    answerBox.style.marginTop = "200px"
+    answerBox.style.marginTop = "160px"
     answerBox.style.background = "black"
     answerBox.style.width = "1200px"
     answerBox.style.height = "240px"
-
+    
+    typeArea.setAttribute("type", "text", "placeholder", "ARE YOU READY?")
+    timeOut()
     typeArea.style.fontSize = "65px"
     typeArea.style.width = "1000px"
     typeArea.style.margin = "7% 0 0 8%"
     typeArea.style.background = "black"
     typeArea.style.color = "white"
     typeArea.style.textAlign = "center"
-    typeArea.style.border = "none"
+    typeArea.style.border = "1px solid black"
+    typeArea.style.outline = "none"    
 }
-// typeArea.addEventListener('click', () => {
-//     typeArea.style.border = "none"
-// })
+
+const timeOut = () => {
+    typeArea.setAttribute("disabled", "disabled")
+    if (typeArea.placeholder = "ARE YOU READY?") {
+        setTimeout( () => {typeArea.placeholder = "3"}, 2000);
+        setTimeout( () => {typeArea.placeholder = "2"}, 3000);
+        setTimeout( () => {typeArea.placeholder = "1"}, 4000);
+        setTimeout( () => {typeArea.placeholder = "START"}, 5000);
+        setTimeout( () => {typeArea.placeholder = "NUMBER HERE"}, 6000);
+        setTimeout( () => {typeArea.removeAttribute("disabled", "disabled")}, 6000);
+    }
+}
 
 const timeRemaining = () => {
+
 }
+
+
+
 
 startBtn.addEventListener('click', () => {
     styleChange()
+    
+
 })
